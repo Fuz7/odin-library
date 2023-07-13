bd961cd9a0fedb7aba4dd4b6885474669e56aede
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2023-07-13 15:56:19 +0800

    Make addtolibrary function & book constructor

--- library.js
+++ library.js
@@ -1,16 +1,24 @@
 let navbar = document.getElementById('nav')
 let sidebar = document.getElementById('info')
 let mainContainer = document.getElementById('mainContainer')
+let cardContainer = document.getElementById('cardContainer')
 
 let form = document.getElementById('form')
 let submitButton = document.getElementById('menuAdd')
+
+let titleInput = document.getElementById('addTitle')
+let authorInput = document.getElementById('addAuthor')
 let totalPage = document.getElementById('totalPage')
-let completedPage = document.getElementById('addCompleted')
+let currentPage = document.getElementById('addCurrent')
+
 let completedLog = document.getElementById('completedLog')
 let menuContainer = document.getElementById('addContainer')
 let menuButton = document.getElementById('addButton')
 let cancelButton = document.getElementById('menuDelete')
 
+let card = document.getElementsByClassName('card')
+card = Array.from(card)
+let bookIndex = Number((card[card.length - 1]).getAttribute('data-book')) +  1
 
 // Menu authentication // -->
 
@@ -20,30 +28,43 @@ totalPage.addEventListener('input', function(){
 
 
 
-completedPage.addEventListener('input', function(){
+currentPage.addEventListener('input', function(){
     toggleLog()
 })
 
 function toggleLog(){
-    if (totalPage.valueAsNumber > completedPage.valueAsNumber){
+    if (totalPage.valueAsNumber < currentPage.valueAsNumber){
         completedLog.classList.add('invalid')
-    } else if(totalPage.valueAsNumber <= completedPage.valueAsNumber){
+    } else if(totalPage.valueAsNumber >= currentPage.valueAsNumber){
         completedLog.classList.remove('invalid')
-        completedPage.setCustomValidity('')
+        currentPage.setCustomValidity('')
     }
 }
 
 submitButton.addEventListener('click', function(e) {
     e.preventDefault()
     if(form.checkValidity() && (!completedLog.classList.contains('invalid')) ){
-        form.submit()
+
+        let newBook = new Book(titleInput.value,authorInput.value,
+            totalPage.valueAsNumber,currentPage.valueAsNumber,bookIndex)
+        addToLibrary(newBook)
+        bookIndex++
+        closeMenu()
+
     } else if(completedLog.classList.contains('invalid')){
-        completedPage.setCustomValidity('Completed page is higher than total page')
+        currentPage.setCustomValidity('Completed page is higher than total page')
         form.reportValidity()
     } else{
         form.reportValidity()
     }
 })
+
+function emptyAddInput(){
+    titleInput.value = "";
+    authorInput.value = "";
+    totalPage.value = "";
+    currentPage.value= ""
+}
 
 // Menu authenthication // <--
 
@@ -63,3 +84,86 @@ menuButton.addEventListener('click', () => openMenu())
 cancelButton.addEventListener('click', () => closeMenu())
 
 // Menu Opening & Closing // <--
+
+// Creating Book // -->
+
+function Book(title, author, totalPage, currentPage, bookIndex){
+    this.title = title;
+    this.author = author;
+    this.totalPage = totalPage;
+    this.currentPage = currentPage;
+    this.bookIndex = bookIndex;
+}
+
+
+function addToLibrary(newBook){
+    let div = document.createElement('div') 
+    div.setAttribute('class', 'card')
+    div.setAttribute('data-book', newBook.bookIndex)
+
+    // for the cards layout
+    let deleteButton = document.createElement('button')
+    deleteButton.setAttribute('class', 'deleteCard')
+    deleteButton.innerHTML = 'Delete'
+    div.append(deleteButton)
+
+
+    let cardContent = document.createElement('div')
+    cardContent.setAttribute('class', 'cardContent')
+
+    let titleContent = document.createElement('div')
+    titleContent.setAttribute('class', 'titleContent')
+    titleContent.innerHTML = newBook.title
+    let authorContent = document.createElement('div')
+    authorContent.setAttribute('class', 'authorContent')
+    authorContent.innerHTML = newBook.author
+    cardContent.append(titleContent)
+    cardContent.append(authorContent)
+
+    div.append(cardContent)
+
+
+    let minusButton = document.createElement('button')
+    minusButton.setAttribute('class', 'minusButton')
+    minusButton.innerHTML = "-"
+    let plusButton = document.createElement('button')
+    plusButton.setAttribute('class', 'plusButton')
+    plusButton.innerHTML = "+"
+    let readButton = document.createElement('button')
+    readButton.setAttribute('class', 'readButton')
+    readButton.innerHTML = "Mark as Read"
+    div.append(minusButton)
+    div.append(plusButton)
+    div.append(readButton)
+    
+    
+    let pageContent = document.createElement('div')
+    pageContent.setAttribute('class', 'pageContent')
+    
+    let currentMarker = document.createElement('div')
+    currentMarker.setAttribute('class', 'currentMarker' )
+    currentMarker.innerHTML = newBook.currentPage
+    let separator = document.createElement('div')
+    separator.innerHTML = "|"
+    let totalMarker = document.createElement('div')
+    totalMarker.setAttribute('class', 'totalMarker')
+    totalMarker.innerHTML = newBook.totalPage
+    pageContent.append(currentMarker)
+    pageContent.append(separator)
+    pageContent.append(totalMarker) 
+
+    div.append(pageContent)
+
+
+    let editButton = document.createElement('button')
+    editButton.setAttribute('class', 'editButton')
+    editButton.innerHTML = "Edit"
+    div.append(editButton)
+
+
+    cardContainer.append(div)
+
+
+}
+
+// Creating Book // <--
